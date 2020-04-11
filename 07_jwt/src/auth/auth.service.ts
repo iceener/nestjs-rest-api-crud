8b4abbaef0114e82da0c9bcdd3f4656cbd4655a7
Adam Gospodarczyk <adam@overment.com>
2020-04-11 15:00:41 +0200

    (hotfix) Fixed bcrypt passport comparsion

--- 07_jwt/src/auth/auth.service.ts
+++ 07_jwt/src/auth/auth.service.ts
@@ -12,7 +12,7 @@ export class AuthService {
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(username);
-    if (user && bcrypt.compare(pass, user.password)) {
+    if (user && await bcrypt.compare(pass, user.password)) {
       const { password, ...result } = user;
       return result;
     }
